f192558e10775c9e025d3f2e65c2b02931ef1f26
Alper Sahin <alpersahin5426@gmail.com>
2026-04-15 18:52:39 +0300

    Final fixes and tests passed

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,4 +1,36 @@
 package org.example;
 
 public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double height;
+    private int weight;
+    private  String hair;
+    public  Person(String firstName, String lastName, int age){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.age=age;
+    }
+    public Person(String firstName, String lastName,int age, double height, int weight, String hair){
+        this(firstName,lastName,age);
+        this.height=height;
+        this.weight=weight;
+        this.hair=hair;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public int getAge(){
+        return age;
+    }
+    public boolean isTeen(){
+        if (age>=13 && age<=19){
+            return true;
+        }
+        return false;
+    }
 }
